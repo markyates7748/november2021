d22c8b6e28afccccf571b7168104b7a84e167e23
Mark Yates <mark.yates@smoothstack.com>
2021-11-02 15:51:36 -0700

    Added exception handling for non-integer input

--- java_basics/src/java_basics_day_two/Assign1.java
+++ java_basics/src/java_basics_day_two/Assign1.java
@@ -3,15 +3,19 @@ package java_basics_day_two;
 public class Assign1 {
 
 	public static void main(String[] args) {
-		
+
 		// TODO take values from the command line and add them together
 		int sum = 0;
-		
-		for(String s: args) {
-			sum += Integer.parseInt(s);
+
+		for(String s : args) {
+			try {
+				sum += Integer.parseInt(s);
+			} catch (NumberFormatException nfe) {
+				System.out.printf("Input '%s' ignored: not an int\n", s);
+			}
 		}
-		
-		System.out.println(sum);
+
+		System.out.println("Sum = " + sum);
 
 	}
 
