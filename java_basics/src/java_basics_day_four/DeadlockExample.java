b930deabe989cae10745694bc877d05ddb89cbe2
Mark Yates <mark.yates@smoothstack.com>
2021-11-04 10:54:00 -0700

    Additional work on day 4 assignments

---
+++ java_basics/src/java_basics_day_four/DeadlockExample.java
@@ -0,0 +1,41 @@
+package java_basics_day_four;
+
+public class DeadlockExample {
+
+	private static Object resourceA = new Object();
+	private static Object resourceB = new Object();
+
+	public static void main(String[] args) {
+		// TODO Create a deadlock between two threads
+		SampleThreadOne thread1 = new SampleThreadOne();
+		SampleThreadTwo thread2 = new SampleThreadTwo();
+
+		thread1.start();
+		thread2.start();
+	}
+
+	private static class SampleThreadOne extends Thread {
+		@Override
+		public void run() {
+			synchronized (resourceA) {
+				System.out.println("Thread One holding Resource A and waiting for Resource B");
+				synchronized (resourceB) {
+					System.out.println("Thread One has Resource A and Resource B");
+				}
+			}
+		}
+	}
+
+	private static class SampleThreadTwo extends Thread {
+		@Override
+		public void run() {
+			synchronized (resourceB) {
+				System.out.println("Thread Two holding Resource B and waiting for Resource A");
+				synchronized (resourceA) {
+					System.out.println("Thread Two has Resource B and Resource A");
+				}
+			}
+		}
+	}
+
+}
---
+++ java_basics/src/java_basics_day_four/ProducerConsumerExample.java
@@ -0,0 +1,37 @@
+package java_basics_day_four;
+
+public class ProducerConsumerExample {
+
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Create producer and consumer threads for ints with a bounded buffer (array)
+		final ProduceConsumeThread example = new ProduceConsumeThread();
+		
+		Thread producer = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					example.produce();
+				}catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		Thread consumer = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					example.consume();
+				}catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		producer.start();
+		consumer.start();
+		
+		producer.join();
+		consumer.join();
+	}
+
+}
