a56f2cfa0c5d25d0543976681d11f14b877cd0f3
Mark Yates <mark.yates@smoothstack.com>
2021-11-17 10:34:03 -0700

    added numpy classwork to end of assignment

--- python_basics/assignment_six_numpy.py
+++ python_basics/assignment_six_numpy.py
@@ -12,6 +12,7 @@ ones_array = np.ones(10)
 print(ones_array)
 
 # 4: Array of 10 fives
+#fives_array = np.full(10,7)
 fives_array = np.ones(10) * 5
 print(fives_array)
 
@@ -34,3 +35,58 @@ print(identity_matrix)
 # 9: Generate random number between 0 and 1
 rand_val = np.random.rand(1)
 print(rand_val)
+
+## Additional numpy classwork
+
+# create array of 25 random numbers
+rand_array = np.random.randn(25)
+print(rand_array)
+
+# 10x10 matrix 0 to 1
+#matrix_ten_by_ten = np.linspace(0,1,100).reshape(10,10)
+matrix_ten_by_ten = np.arange(1,101).reshape(10,10) / 100
+print(matrix_ten_by_ten)
+
+# array of 20 linearly spaced points between 0 and 1
+array_linear_spaced = np.linspace(0,1,20)
+print(array_linear_spaced)
+
+# splice given matrix according to given instructions
+original_matrix = np.arange(1,26).reshape(5,5)
+print(original_matrix)
+
+# select rows 3 onwards, columns 2 onwards
+matrix_splice_one = original_matrix[2:,1:]
+print(matrix_splice_one)
+
+# select entry at (4,5)
+matrix_splice_two = original_matrix[3,4]
+print(matrix_splice_two)
+
+# select rows after 3, column 2 as unique rows
+matrix_splice_three = original_matrix[:3,1:2]
+print(matrix_splice_three)
+
+# select all from row 5
+matrix_splice_four = original_matrix[4,:]
+print(matrix_splice_four)
+
+# select all from rows 4 to 5
+matrix_splice_five = original_matrix[3:5,:]
+print(matrix_splice_five)
+
+# sum values in given matrix
+matrix_sum = original_matrix.sum()
+print(matrix_sum)
+
+# get std dev of values in given matrix
+matrix_std_dev = original_matrix.std()
+print(matrix_std_dev)
+
+# get sum of columns in given matrix
+column_sum = original_matrix.sum(axis=0)
+print(column_sum)
+
+# get sum of rows in given matrix
+row_sum = original_matrix.sum(axis=1)
+print(row_sum)
